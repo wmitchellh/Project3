be44f61854fb0a7db306bd5b645213fecd73a2b5
wmitchellh <54546794+wmitchellh@users.noreply.github.com>
2019-10-22 19:35:47 -0400

    2nd commit - unfinished

--- Project 3/js/main.js
+++ Project 3/js/main.js
@@ -1,65 +1,93 @@
-var data;
-var tenant_name = [];
-var years_in_business = [];
-var downtown_district = [];
 
 $(document).ready(function(){
   $('#table').dataTable( {
     "ajaxSource": "downtown-tenant.json",
     "columns": [
-      { "data": "date_closed" },
-      { "data": "years_in_business" },
-      { "data": "website" },
-      { "data": "address_number" },
-      { "data": "date_updated" },
       { "data": "tenant_name" },
-      { "data": "street_name" },
-      { "data": "address" },
-      { "data": "naics_code" },
-      { "data": "independent_v_chain" },
       { "data": "category_of_use" },
       { "data": "downtown_district" },
-      { "data": "date_opened" },
+      { "data": "years_in_business" },
+      { "data": "independent_v_chain" },
+      { "data": "address" },
     ]
   } );
 
-  loadData();
 });
 
-function loadData(){
+// set the dimensions of the canvas
+var margin = {top: 40, right: 20, bottom: 200, left: 200},
+    width = 800 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
-    $.ajax({
-            type:"GET",
-            url:"downtown-tenant.json",
-            dataType:"json",
-            success: parseData
+
+// set the ranges
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+var y = d3.scale.linear().range([height, 0]);
+
+// define the axis
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
+
+
+// add the SVG element
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+// load the data
+d3.json("years.json", function(error, data) {
+
+    data.forEach(function(d) {
+        d.tenant_name = d.tenant_name;
+        d.years_in_business = +d.years_in_business;
+    });
+
+  // scale the range of the data
+  x.domain(data.map(function(d) { return d.tenant_name; }));
+  y.domain([0, d3.max(data, function(d) { return d.years_in_business; })]);
+
+  // add axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" );
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 5)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Years in Business");
+
+
+  // Add bar chart
+  svg.selectAll("bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.tenant_name); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.years_in_business); })
+      .attr("height", function(d) { return height - y(d.years_in_business); });
+
 });
-
-
-}
-
-
-function parseData(data){
-    console.log(data);
-     for (var i = 0, len = data.length; i < len; ++i) {
-            //sets data to arrays for charts
-            // tenant_name.push([i]["tenant_name"]);
-            years_in_business.push([i]["years_in_business"]);
-            // downtown_district.push([i]["downtown_district"]);
-     }
-    createCharts();
-}
-
-function createCharts() {
-
-  setTimeout(function () {
-      c3.generate({
-        bindto: '#column-chart',
-          data: {
-              url: 'downtown-tenant.json',
-              mimeType: 'json',
-          }
-      });
-  }, 1000);
-
-}
